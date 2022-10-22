e3cdd18e803312a4d73d1cd17359d280a0013439
Chidobe <thedobecompany@gmail.com>
2022-10-22 15:43:19 +0100

    setting up the photo section to change height with the dog pictures

--- api.js
+++ api.js
@@ -1,18 +1,18 @@
 
 const section = document.querySelector('section')
 let image = document.createElement('img')
-
-fetch("https://dog.ceo/api/breeds/image/random")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        image.src = data.message;
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+image.setAttribute('style', 'width: 350px; height: auto;')
 
 const btn = document.querySelector('button')
 btn.addEventListener('click', () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            image.src = data.message;
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
     section.append(image);
-}, {once : false});
+});
